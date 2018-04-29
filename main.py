4294beeb8534736348ef003098d0f6f5552416dc
jefftianyuliu <jeff_tianyu_liu@126.com>
2018-04-29 07:50:28 +0000

    coporated main with passing

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from Tkinter import *
 import Tkinter as tk
 from PIL import ImageTk, Image
 from tkFileDialog import askopenfilename, askdirectory
+from pwn import *
+import time
+import string
+import os
+import fnmatch
+import psutil
 
 #class for file table
 class SimpleTable(tk.Frame):
@@ -26,8 +32,8 @@ font = "Helvetica 12 bold italic")
                 self._widgets.append(current_row)
                 continue
             for column in range(columns):
-                label = tk.Label(self, text="", borderwidth=1, width=45, fg = 'black',
-font = "Helvetica 10 bold italic",bg = 'white')
+                label = tk.Label(self, text="", borderwidth=1, width=40, fg = 'black',
+font = "Arial 10",bg = 'white',anchor=W)
                 label.grid(row=row, column=column, sticky="nswe", padx=1, pady=1)
                 current_row.append(label)
             self._widgets.append(current_row)
@@ -50,6 +56,8 @@ class simpleform_ap(Tk):
         self.disk_image_path = StringVar()
         self.file_type = StringVar()
         self.dir_output_path = StringVar()
+        self.percentage = StringVar()
+        self.num_files_recovered = StringVar()
         self.input_path = ""
         self.output_path = ""
         self.conf_path = "JPG/GIF/PNG"
@@ -64,8 +72,54 @@ class simpleform_ap(Tk):
         self.other_flags = ""
         #text box for commands
         self.entry_text = StringVar()
+        self.commands_to_execute = ""
+        self.t = SimpleTable(self, 16, 2)
         self.initialize()
         self.grid()
+        self.pss = 0
+        # the scalpel command received from GUI
+        self.cmd = ""
+        # status of executing the command
+        # -1: command itself constructed with errors
+        # 0: command executed correctly
+        # 1: command executed with error
+        self.status = -2
+        # Scalpel version
+        self.version = ""
+        # Author Informaiton
+        self.authorInfo = ""
+        # Images to be carved
+        self.target = ""
+
+        ##### TODO, multiple images support #####
+
+        # percentage info for allocation, contains two float numbers:
+        # 1, percentage info as NUM, such as 4.9 %
+        # 2, bytes carved as NUM, such as 10.0 MB
+        self.allocating_queue = ["", ""]
+        # Carving signature list, contains four strings:
+        # 1, type of carving files, such as "gif", "jpg"
+        # 2, header, such as "\x47\x49\x46\x38\x37\x61"
+        # 3, footer, such as "\x00\x3b"
+        # 4, number of files carved for this signature, such as "20" files
+        self.carving_list = []
+        # percentage info for processing of images, contains two float numbers:
+        # 1, percentage info as NUM, such as 48.8 %
+        # 2, bytes carved as NUM, such as 100.0 MB
+        self.image_processing = ["", ""]
+
+        ##### TODO                          #####
+
+        # Total number of file carved
+        self.num_file_carving = -1
+        # Total time consumed
+        self.time_consumed = -1
+        # Error informaiton if occured
+        self.error_info = ""
+        # MD5 list of all carved files
+        self.md5list = {}
+
+
 
 
     #############################GUI Initialization###################################
@@ -130,12 +184,16 @@ variable=self.extension, command=self.activateextension, font = "Helvetica 10 it
 
 
         #Recover Percentage
-        label_5 = Label(self, text = "Progress: 0.0%", fg = 'black', 
-font = "Helvetica 14 bold italic").place(rely=1.0, relx=0, x=5, y=-75, anchor=SW) #Label 
+        self.percentage.set("Progress: 0.0% processed, 0 MB carved")
+        label_5 = Label(self, textvariable = self.percentage, fg = 'black', 
+font = "Helvetica 14 bold italic").place(rely=1.0, relx=0, x=5, y=-105, anchor=SW)
+        self.num_files_recovered.set("Number of files recovered: 0")
+        label_6 = Label(self, textvariable = self.num_files_recovered , fg = 'black',
+font = "Helvetica 14 bold italic").place(rely=1.0, relx=0, x=5, y=-75, anchor=SW)
 
         #Text box for commands
-        label_5 = Label(self, text = "scalpel commands:", fg = 'black', 
-font = "Helvetica 11 bold italic").place(rely=1.0, relx=0, x=5, y=-45, anchor=SW) #Label 
+        label_7 = Label(self, text = "scalpel commands:", fg = 'black', 
+font = "Helvetica 12 bold italic").place(rely=1.0, relx=0, x=5, y=-45, anchor=SW) #Label 
         large_font = ('Verdana',9)
         entry = Entry(self, textvariable=self.entry_text,font=large_font)
         entry.configure(background = "black", width = 95, foreground="white")
@@ -148,10 +206,9 @@ font = "Helvetica 14 bold italic")
         rbutton.place(rely=1.0, relx=1.0, x=-10, y=-15, anchor=SE) 
 
         #Table for recovered files
-        t = SimpleTable(self, 16, 2)
-        t.pack(side="right", fill="x", anchor="n")
-        t.set(1,0,"Carved Files (First 15)") 
-        t.set(1,1,"MD5 Hash") 
+        self.t.pack(side="right", fill="x", anchor="n")
+        self.t.set(1,0,"Carved Files (First 15)") 
+        self.t.set(1,1,"MD5 Hash") 
 
 
     #############################Event Handlers###################################
@@ -212,6 +269,8 @@ font = "Helvetica 14 bold italic")
             tkMessageBox.showerror("Error", "No output directory selected!")
             return
         self.updatecommands()
+        self.executecommands()
+
 
     #ignore footer
     def activatefooter(self):
@@ -243,7 +302,189 @@ font = "Helvetica 14 bold italic")
     def updatecommands(self):
         self.updateflags()
         commands = "scalpel -c " + self.conf_filename + self.other_flags + " " + self.input_path + " -o " + self.output_path
-        self.entry_text.set(commands)
+        self.commands_to_execute = commands
+        self.entry_text.set(self.commands_to_execute)
+    def executecommands(self):
+        self.emptyfiletable()
+        self.cmd = self.commands_to_execute
+        self.entry_text.set("")
+        self.update()
+        self.execute()
+        if self.status == 0:
+           self.num_files_recovered.set("Number of files recovered: " + str(self.num_file_carving))
+           self.updatefiletable()
+           return
+        if self.status == 1:
+           tkMessageBox.showerror("Error", self.error_info)
+           return
+        if self.status == -1:
+           tkMessageBox.showerror("Error", "Invalid Scalpel Commands: "+ self.commands_to_execute)
+           return
+    def updatefiletable(self):
+        count = 2
+        for key,value in self.md5list.iteritems():
+            y = key.replace(self.output_path,"")
+            self.t.set(count,0,y)
+            self.t.set(count,1,value)
+            count = count + 1
+            if count >= 16:
+                break
+        self.update()
+    def emptyfiletable(self):
+        for count in range(2,16):
+            self.t.set(count,0,"")
+            self.t.set(count,1,"")
+        self.update()
+
+    ###################Functions from Passing.py#############################################
+    # Main function to execute and parse result
+    def execute(self):
+        self.pss = 0
+        # the scalpel command received from GUI
+        # status of executing the command
+        # -1: command itself constructed with errors
+        # 0: command executed correctly
+        # 1: command executed with error
+        self.status = -2
+        # Scalpel version
+        self.version = ""
+        # Author Informaiton
+        self.authorInfo = ""
+        # Images to be carved
+        self.target = ""
+
+        ##### TODO, multiple images support #####
+
+        # percentage info for allocation, contains two float numbers:
+        # 1, percentage info as NUM, such as 4.9 %
+        # 2, bytes carved as NUM, such as 10.0 MB
+        self.allocating_queue = ["", ""]
+        # Carving signature list, contains four strings:
+        # 1, type of carving files, such as "gif", "jpg"
+        # 2, header, such as "\x47\x49\x46\x38\x37\x61"
+        # 3, footer, such as "\x00\x3b"
+        # 4, number of files carved for this signature, such as "20" files
+        self.carving_list = []
+        # percentage info for processing of images, contains two float numbers:
+        # 1, percentage info as NUM, such as 48.8 %
+        # 2, bytes carved as NUM, such as 100.0 MB
+        self.image_processing = ["", ""]
+
+        ##### TODO                          #####
+
+        # Total number of file carved
+        self.num_file_carving = -1
+        # Total time consumed
+        self.time_consumed = -1
+        # Error informaiton if occured
+        self.error_info = ""
+        # MD5 list of all carved files
+        self.md5list = {}
+        if "scalpel" not in self.cmd:
+            self.status = -1
+            return
+        else:
+            p = process(self.cmd, shell=True)
+            process_checker = 1
+            front = p.recvlines(5)
+            path = ""
+
+            # Assign version and author information
+            for line in front:
+                if "Scalpel version" in line:
+                    self.version = line.split(" ")[2].strip()
+                if "Golden G. Richard III" in line:
+                    self.authorInfo = line
+
+            if "ERROR:" in front[2]:
+                for line in front[2:]:
+                    self.error_info += line + "\n"
+                self.status = 1
+                return
+            # Assign images to be processed
+            else:
+                self.status = 0
+                for line in front:
+                    if "Opening target" in line:
+                        self.target = line.split(" ")[2].strip('"')
+                    else:
+                        continue
+
+            while p.can_recv():
+                result = ""
+                try:
+                    result = p.recvuntil('\r')
+                    if "ETA" in result and "Allocating work queues" not in result and "Processing of image" not in result:
+                        percentage = float(result.split(":")[1].split("%")[0].strip())
+                        byte = float(result.split(":")[1].split("%")[1].split("MB")[0].strip())
+                        if process_checker == 1:
+                            self.allocating_queue[0] = percentage
+                            self.allocating_queue[1] = byte
+                            self.percentage.set("Progress: " + str(self.allocating_queue[0]) + "% processed, " + str(self.allocating_queue[1]) + " MB carved")
+                            self.update()
+                        else:
+                            self.image_processing[0] = percentage
+                            self.image_processing[1] = byte
+                            self.percentage.set("Progress: " + str(self.allocating_queue[0]) + "% processed, " + str(self.allocating_queue[1]) + " MB carved")
+                            self.update()
+                            if percentage > 99.9:
+                                break
+                    elif "Allocating work queues" in result:
+                        percentage = float(result.splitlines()[0].split(":")[1].split("%")[0].strip())
+                        byte = float(result.splitlines()[0].split(":")[1].split("%")[1].split("MB")[0].strip())
+                        if process_checker == 1:
+                            self.allocating_queue[0] = percentage
+                            self.allocating_queue[1] = byte
+                            self.percentage.set("Progress: " + str(self.allocating_queue[0]) + "% processed, " + str(self.allocating_queue[1]) + " MB carved")
+                            self.update()
+                            process_checker = 2
+
+                        for line in result.splitlines()[1:]:
+                            if "header" in line and "footer" in line:
+                                temp = []
+                                temp.append(line.split(" ")[0].strip())
+                                temp.append(line.split(" ")[3].strip('"'))
+                                temp.append(line.split(" ")[6].strip('"'))
+                                temp.append(line.split(" ")[8].strip())
+                                self.carving_list.append(temp)
+                    else:
+                        continue
+
+                except EOFError:
+                    break
+            # Handle the rest of parsing info
+            end = p.recvall()
+            for line in end.splitlines():
+                if "Processing of image" in line:
+                    self.image_processing[0] = float(line.split(":")[1].split("%")[0].strip())
+                    self.image_processing[1] = float(line.split(":")[1].split("%")[1].split("MB")[0].strip())
+                elif "Scalpel is done," in line:
+                    self.num_file_carving = int(line.split(" ")[6].strip(','))
+                    self.time_consumed = int(line.split(" ")[9].strip())
+                else:
+                    continue
+
+            if self.status == 0:
+                parameter = self.cmd.split(" ")
+                for i in range(0,len(parameter)):
+                    if parameter[i] == "-o":
+                        path = parameter[i+1]
+                        break
+                for root, dirs, files in os.walk(path):
+                    for file in files:
+                        if file == "audit.txt":
+                            continue
+                        else:
+                            filename = os.path.join(root, file)
+                            self.md5list[filename] = self.md5(filename)
+
+    def md5(self, fname):
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        f.close()
+        return hash_md5.hexdigest()
 
 
 
